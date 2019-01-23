ff82b5199ea1d1bcd97d62994a903e228d599dc8
Åke Engelbrektson <eson57@users.noreply.github.com>
2019-01-23 07:25:59 +0100

    Create harbour-vodman-sv.ts
    Swedish translation if wanted.

---
+++ src/vodman-app/translations/harbour-vodman-sv.ts
@@ -0,0 +1,365 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="format-label">
+        <location filename="../qml/FormatComboBox.qml" line="35"/>
+        <location filename="../qml/pages/SelectFormatDialog.qml" line="89"/>
+        <location filename="../qml/pages/SettingsPage.qml" line="167"/>
+        <source>Format</source>
+        <translation>Format</translation>
+    </message>
+    <message id="format-best">
+        <location filename="../qml/FormatComboBox.qml" line="41"/>
+        <source>best quality (largest)</source>
+        <translation>bästa kvalitet (störst)</translation>
+    </message>
+    <message id="format-worst">
+        <location filename="../qml/FormatComboBox.qml" line="45"/>
+        <source>worst quality (smallest)</source>
+        <translation>sämsta kvalitet (minst)</translation>
+    </message>
+    <message id="format-combobox-ask">
+        <location filename="../qml/FormatComboBox.qml" line="54"/>
+        <source>ask every time</source>
+        <translation>fråga varje gång</translation>
+    </message>
+    <message id="about-page-header">
+        <location filename="../qml/pages/AboutPage.qml" line="48"/>
+        <source>About Vodman</source>
+        <extracomment>About Vodman</extracomment>
+        <translation>Om Vodman</translation>
+    </message>
+    <message id="about-version-text">
+        <location filename="../qml/pages/AboutPage.qml" line="93"/>
+        <source>Vodman 1.X.Y</source>
+        <translation>Vodman 1.X.Y</translation>
+    </message>
+    <message id="about-description-header">
+        <location filename="../qml/pages/AboutPage.qml" line="118"/>
+        <source>Description</source>
+        <translation>Beskrivning</translation>
+    </message>
+    <message id="about-description-text">
+        <location filename="../qml/pages/AboutPage.qml" line="125"/>
+        <source>%1 is a versatile tool...</source>
+        <oldsource>%1 is a versatile tool for downloading videos on demand (VODs) to your device from popular sites such as &lt;a href=\&quot;https://www.youtube.com/\&quot;&gt;Youtube&lt;/a&gt; or &lt;a href=\&quot;https://www.twitch.tv/\&quot;&gt;Twitch&lt;/a&gt;.</oldsource>
+        <translation>%1 är ett mångsidigt verktyg...</translation>
+    </message>
+    <message id="about-licensing-header">
+        <location filename="../qml/pages/AboutPage.qml" line="135"/>
+        <source>Licensing</source>
+        <translation>Licensiering</translation>
+    </message>
+    <message id="about-licensing-text">
+        <location filename="../qml/pages/AboutPage.qml" line="142"/>
+        <source>Copyright (c) 2018, 2019 Jean Gressmann.</source>
+        <translation>Copyright (c) 2018, 2019 Jean Gressmann.</translation>
+    </message>
+    <message id="about-sources-header">
+        <location filename="../qml/pages/AboutPage.qml" line="152"/>
+        <source>Sources</source>
+        <translation>Källkod</translation>
+    </message>
+    <message id="about-sources-text">
+        <location filename="../qml/pages/AboutPage.qml" line="159"/>
+        <source>Sources are on Github</source>
+        <translation>Källkod finns på GitHub</translation>
+    </message>
+    <message id="error-youtube-dl-not-working">
+        <location filename="../qml/pages/DownloadPage.qml" line="190"/>
+        <source>youtube-dl not working</source>
+        <translation>youtube-dl fungerar inte</translation>
+    </message>
+    <message id="error-youtube-dl-process-crashed">
+        <location filename="../qml/pages/DownloadPage.qml" line="195"/>
+        <source>youtube-dl process crashed</source>
+        <translation>youtube-dl-processen kraschade</translation>
+    </message>
+    <message id="error-download-timed-out">
+        <location filename="../qml/pages/DownloadPage.qml" line="200"/>
+        <source>Download timed out</source>
+        <translation>Tidsgränsen för nerladdning överskreds</translation>
+    </message>
+    <message id="error-invalid-response">
+        <location filename="../qml/pages/DownloadPage.qml" line="205"/>
+        <source>Host delivered an invalid response to VOD format request</source>
+        <translation>Värddatorn returnerade ett ogiltigt svar på VOD-formatbegäran</translation>
+    </message>
+    <message id="error-unsupported-url">
+        <location filename="../qml/pages/DownloadPage.qml" line="210"/>
+        <source>youtube-dl doesn&apos;t know how to handle that URL</source>
+        <translation>youtube-dl kan inte hantera denna URL</translation>
+    </message>
+    <message id="error-no-video">
+        <location filename="../qml/pages/DownloadPage.qml" line="215"/>
+        <source>No video content for URL</source>
+        <translation>Inget videoinnehåll på denna URL</translation>
+    </message>
+    <message id="error-format-not-available">
+        <location filename="../qml/pages/DownloadPage.qml" line="220"/>
+        <source>The format you selected is not available. Try again or choose another format.</source>
+        <translation>Det valda formatet finns inte tillgängligt. Försök igen eller välj ett annat format.</translation>
+    </message>
+    <message id="error-network-down">
+        <location filename="../qml/pages/DownloadPage.qml" line="225"/>
+        <source>Network down.</source>
+        <translation>Nätverk nere.</translation>
+    </message>
+    <message id="error-invalid-url">
+        <location filename="../qml/pages/DownloadPage.qml" line="230"/>
+        <source>Invalid URL.</source>
+        <translation>Ogiltig URL.</translation>
+    </message>
+    <message id="error-no-space">
+        <location filename="../qml/pages/DownloadPage.qml" line="235"/>
+        <source>No space left on device.</source>
+        <translation>Inget ledigt utrymme på enheten.</translation>
+    </message>
+    <message id="error-already-downloading">
+        <location filename="../qml/pages/DownloadPage.qml" line="240"/>
+        <source>Already downloading %1</source>
+        <translation>%1 laddas redan ner</translation>
+    </message>
+    <message id="error-unknown">
+        <location filename="../qml/pages/DownloadPage.qml" line="245"/>
+        <source>Yikes! An unknown error has occured :/</source>
+        <translation>Ett okänt fel har inträffat :/</translation>
+    </message>
+    <message id="play">
+        <location filename="../qml/pages/DownloadPage.qml" line="258"/>
+        <location filename="../qml/pages/DownloadPage.qml" line="622"/>
+        <source>Play</source>
+        <translation>Spela</translation>
+    </message>
+    <message id="nofification-download-failed-summary">
+        <location filename="../qml/pages/DownloadPage.qml" line="292"/>
+        <location filename="../qml/pages/DownloadPage.qml" line="294"/>
+        <source>Download failed</source>
+        <translation>Nerladdning mysslyckades</translation>
+    </message>
+    <message id="nofification-download-finished-summary">
+        <location filename="../qml/pages/DownloadPage.qml" line="303"/>
+        <location filename="../qml/pages/DownloadPage.qml" line="305"/>
+        <source>Download finished</source>
+        <translation>Nerladdning slutförd</translation>
+    </message>
+    <message id="menu-item-about-vodman">
+        <location filename="../qml/pages/DownloadPage.qml" line="316"/>
+        <source>About Vodman</source>
+        <translation>Om Vodman</translation>
+    </message>
+    <message id="menu-item-settings">
+        <location filename="../qml/pages/DownloadPage.qml" line="322"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message id="menu-item-clear-clipboard">
+        <location filename="../qml/pages/DownloadPage.qml" line="380"/>
+        <source>Clear clipboard</source>
+        <translation>Rensa urklipp</translation>
+    </message>
+    <message id="menu-item-cancel-all-downloads">
+        <location filename="../qml/pages/DownloadPage.qml" line="389"/>
+        <source>Cancel all downloads</source>
+        <translation>Avbryt alla nerladdningar</translation>
+    </message>
+    <message id="remorse-cancel-all-downloads">
+        <location filename="../qml/pages/DownloadPage.qml" line="394"/>
+        <source>Stopping all downloads</source>
+        <translation>Stoppar alla nerladdningar</translation>
+    </message>
+    <message id="menu-item-cancel-all-downloads-delete">
+        <location filename="../qml/pages/DownloadPage.qml" line="401"/>
+        <source>Cancel all downloads and delete files</source>
+        <translation>Avbryt alla nerladdningar och ta bort filerna</translation>
+    </message>
+    <message id="remorse-cancel-all-downloads-delete">
+        <location filename="../qml/pages/DownloadPage.qml" line="406"/>
+        <source>Purging all downloads</source>
+        <translation>Tar bort alla nerladdningar</translation>
+    </message>
+    <message id="cancel">
+        <location filename="../qml/pages/DownloadPage.qml" line="413"/>
+        <location filename="../qml/pages/DownloadPage.qml" line="616"/>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message id="menu-item-download-from-clipboard">
+        <location filename="../qml/pages/DownloadPage.qml" line="420"/>
+        <source>Download from clipboard</source>
+        <translation>Ladda ner från urklipp</translation>
+    </message>
+    <message id="download-page-header">
+        <location filename="../qml/pages/DownloadPage.qml" line="439"/>
+        <source>Downloads</source>
+        <translation>Nerladdningar</translation>
+    </message>
+    <message id="download-item-remorse-cancel">
+        <location filename="../qml/pages/DownloadPage.qml" line="459"/>
+        <source>Stopping %1</source>
+        <translation>Stoppar %1</translation>
+    </message>
+    <message id="download-item-cancel-and-delete">
+        <location filename="../qml/pages/DownloadPage.qml" line="610"/>
+        <source>Cancel and delete file</source>
+        <translation>Avbryt och ta bort filen</translation>
+    </message>
+    <message id="download-item-open-webpage">
+        <location filename="../qml/pages/DownloadPage.qml" line="628"/>
+        <source>Open webpage</source>
+        <translation>Öppna webbsidan</translation>
+    </message>
+    <message id="download-placeholder-text-no-downloads">
+        <location filename="../qml/pages/DownloadPage.qml" line="643"/>
+        <source>No downloads at present</source>
+        <translation>Inga nerladdningar just nu</translation>
+    </message>
+    <message id="download-placeholder-text-metadata-download">
+        <location filename="../qml/pages/DownloadPage.qml" line="647"/>
+        <source>Downloading VOD metadata</source>
+        <translation>Laddar ner VOD metadata</translation>
+    </message>
+    <message id="download-placeholder-hint-pull-down-to-start-download-from-clipboard">
+        <location filename="../qml/pages/DownloadPage.qml" line="654"/>
+        <source>Pull down to start download using the URL in the clipboard</source>
+        <translation>Dra neråt för att starta nerladdning från URL i urklipp</translation>
+    </message>
+    <message id="download-placeholder-hint-copy-url-to-clipboard">
+        <location filename="../qml/pages/DownloadPage.qml" line="658"/>
+        <source>Copy a URL to the clipboard then pull down to start the download</source>
+        <translation>Kopiera URL till urklipp och dra sedan neråt för att starta nerladdningen</translation>
+    </message>
+    <message id="download-placeholder-hint-pull-down-to-cancel">
+        <location filename="../qml/pages/DownloadPage.qml" line="661"/>
+        <source>Pull down to cancel</source>
+        <translation>Dra neråt för att avbryta</translation>
+    </message>
+    <message id="select-format-dialog-title">
+        <location filename="../qml/pages/SelectFormatDialog.qml" line="77"/>
+        <source>Select a format</source>
+        <translation>Välj ett format</translation>
+    </message>
+    <message id="settings-network-header">
+        <location filename="../qml/pages/SettingsPage.qml" line="134"/>
+        <source>Network</source>
+        <translation>Nätverk</translation>
+    </message>
+    <message id="settings-network-connection-type">
+        <location filename="../qml/pages/SettingsPage.qml" line="141"/>
+        <source>Network connection type</source>
+        <translation>Nätverkets anslutningstyp</translation>
+    </message>
+    <message id="settings-network-connection-autodetect">
+        <location filename="../qml/pages/SettingsPage.qml" line="145"/>
+        <source>Autodetect</source>
+        <translation>Identifiera automatiskt</translation>
+    </message>
+    <message id="settings-network-connection-broadband">
+        <location filename="../qml/pages/SettingsPage.qml" line="149"/>
+        <source>Broadband</source>
+        <translation>Bredband</translation>
+    </message>
+    <message id="settings-network-connection-mobile">
+        <location filename="../qml/pages/SettingsPage.qml" line="153"/>
+        <source>Mobile</source>
+        <translation>Mobil</translation>
+    </message>
+    <message id="settings-network-broadband-label">
+        <location filename="../qml/pages/SettingsPage.qml" line="172"/>
+        <source>Broadband</source>
+        <translation>Bredband</translation>
+    </message>
+    <message id="settings-network-mobile-label">
+        <location filename="../qml/pages/SettingsPage.qml" line="183"/>
+        <source>Mobile</source>
+        <translation>Mobil</translation>
+    </message>
+    <message id="settings-save-location-header">
+        <location filename="../qml/pages/SettingsPage.qml" line="194"/>
+        <source>Location</source>
+        <translation>Plats</translation>
+    </message>
+    <message id="settings-save-location-directory-label">
+        <location filename="../qml/pages/SettingsPage.qml" line="205"/>
+        <source>Directory</source>
+        <translation>Mapp</translation>
+    </message>
+    <message id="settings-save-location-directory-downloads">
+        <location filename="../qml/pages/SettingsPage.qml" line="209"/>
+        <source>Downloads</source>
+        <translation>Nerladdningar</translation>
+    </message>
+    <message id="settings-save-location-directory-videos">
+        <location filename="../qml/pages/SettingsPage.qml" line="213"/>
+        <source>Videos</source>
+        <translation>Videor</translation>
+    </message>
+    <message id="settings-save-location-directory-custom">
+        <location filename="../qml/pages/SettingsPage.qml" line="217"/>
+        <source>Custom</source>
+        <translation>Anpassat</translation>
+    </message>
+    <message id="settings-save-location-directory-field-label">
+        <location filename="../qml/pages/SettingsPage.qml" line="227"/>
+        <source>VODs are saved here</source>
+        <translation>VOD:ar sparas här</translation>
+    </message>
+    <message id="settings-save-location-directory-field-placeholder">
+        <location filename="../qml/pages/SettingsPage.qml" line="229"/>
+        <source>Default directory to save VODs</source>
+        <translation>Standardmapp för att spara VOD:ar</translation>
+    </message>
+    <message id="settings-save-location-pick-directory">
+        <location filename="../qml/pages/SettingsPage.qml" line="242"/>
+        <source>Pick a directory</source>
+        <translation>Välj en mapp</translation>
+    </message>
+    <message id="settings-save-location-file-name-combobox-label">
+        <location filename="../qml/pages/SettingsPage.qml" line="271"/>
+        <source>File name</source>
+        <translation>Filnamn</translation>
+    </message>
+    <message id="settings-save-location-file-name-combobox-item-title">
+        <location filename="../qml/pages/SettingsPage.qml" line="275"/>
+        <source>Title</source>
+        <translation>Titel</translation>
+    </message>
+    <message id="settings-save-location-file-name-combobox-item-video-id">
+        <location filename="../qml/pages/SettingsPage.qml" line="279"/>
+        <source>Video id</source>
+        <translation>Video-ID</translation>
+    </message>
+    <message id="settings-save-location-file-name-combobox-item-custom">
+        <location filename="../qml/pages/SettingsPage.qml" line="283"/>
+        <source>Custom</source>
+        <translation>Anpassat</translation>
+    </message>
+    <message id="settings-save-location-file-name-textfield-label">
+        <location filename="../qml/pages/SettingsPage.qml" line="309"/>
+        <source>File name template for VODs</source>
+        <translation>Filnamnsmall för VOD:ar</translation>
+    </message>
+    <message id="settings-save-location-file-name-textfield-placeholder">
+        <location filename="../qml/pages/SettingsPage.qml" line="311"/>
+        <source>File name template for VODs</source>
+        <translation>Filnamnsmall för VOD:ar</translation>
+    </message>
+    <message id="settings-save-location-file-name-description">
+        <location filename="../qml/pages/SettingsPage.qml" line="321"/>
+        <source>Tags {title} {id} {formatid} will be substituded from the VOD&apos;s meta data.</source>
+        <translation>Taggar {title} {id} {formatid}, kommer att hämtas från VOD:ens metadata</translation>
+    </message>
+    <message id="settings-page-header">
+        <location filename="../qml/pages/SettingsPage.qml" line="341"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message id="cover-no-downloads">
+        <location filename="../qml/cover/CoverPage.qml" line="55"/>
+        <source>No downloads at present</source>
+        <translation>Inga nerladdningar just nu</translation>
+    </message>
+</context>
+</TS>
